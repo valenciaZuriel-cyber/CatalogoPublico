5465cbe7d6b03de4424c9686a52cd7019531acce
Zuriel Valencia <valenciavvz0@gmail.com>
2026-06-24 13:11:32 -0600

    Creacion Shema Productos

---
+++ src/Modules/products.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+         required: true,
+    },
+    description:{
+        type: String,
+         required: true,
+    },
+    category: {
+        type: String,
+         required: true,
+    },
+    price: {
+        type: Integer,
+        required: true,
+    },
+    quantity: {
+        type: Integer,
+        required: true,
+    },
+    CreationDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
+
