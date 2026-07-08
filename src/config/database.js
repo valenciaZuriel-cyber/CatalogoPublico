59b1a18889f203795e51659f56bdc05e4c8f367e
Zuriel Valencia <valenciavvz0@gmail.com>
2026-07-08 13:14:43 -0600

    Solucion de error vercel

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Successful connected to MongoDB");
     } catch (err) {
-        console.error("Failed to connect to MongoDB. Message: ", err);
-        process.exit(1); // stop process 
+        console.error("Failed to connect to MongoDB. Message: ", err); 
     }
 }; 
 
